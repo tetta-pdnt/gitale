28ce498d52b90217097c07c8762dfb8704215a55
tetta-pdnt <tetta.noguchi@gmail.com>
2024-06-15 16:05:07 +0900

    Add about/button

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function About() {
+  return (
+    <main>
+        <div>About</div>
+        <Button>Click me</Button>
+    </main>
+  );
+}
